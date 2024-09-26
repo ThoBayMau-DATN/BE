8753dd4a0bde3ceb267c19c9721cbb094240c992
TUAN <hoangquoctuans3@gmail.com>
2024-09-27 00:25:57 +0700

    api dang nhap

---
+++ BACK-END/DTOs/Auth/DangNhapRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BACK_END.DTOs.Auth
+{
+    public class DangNhapRepository
+    {
+        public string HoTen { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string? SDT { get; set; } = string.Empty;
+        public string? Anh { get; set; } = string.Empty;
+        public bool TrangThai { get; set; }
+    }
+
+}
