8068ae0767a2f9470324bc0ec8e2f58899440a0d
TUAN <hoangquoctuans3@gmail.com>
2024-09-26 17:36:17 +0700

    xong kiểm lỗi đăng ký tài khoản

--- BACK-END/Controllers/AuthController.cs
+++ BACK-END/Controllers/AuthController.cs
@@ -9,10 +9,11 @@ namespace BACK_END.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuth _auth;
-
-        public AuthController(IAuth auth)
+        private readonly ILogger<AuthController> _logger;
+        public AuthController(IAuth auth, ILogger<AuthController> logger)
         {
             _auth = auth;
+            _logger = logger;
         }
 
         [HttpPost("register")]
@@ -43,6 +44,51 @@ namespace BACK_END.Controllers
             {
                 return BadRequest(ex.Message);
             }
+        }
+        [HttpPost("dangKyStaff")]
+        public async Task<IActionResult> DangKyUser([FromBody] DangKyUserDto model)
+        {
+            if (!ModelState.IsValid)
+            {
+                var errorMessages = ModelState.Values
+                    .SelectMany(v => v.Errors)
+                    .Select(e => e.ErrorMessage);
+
+                var fullErrorMessage = string.Join("; ", errorMessages);
+
+                return BadRequest(fullErrorMessage);
+
+            }
+
+            try
+            {
+                var checkEmail = await _auth.CheckEmail(model.Email);
+                var checkPhone = await _auth.CheckSDT(model.SoDienThoai);
+
+                if (checkEmail == true)
+                {
+                    return BadRequest("Email đã tồn tại.");
+                }
+                if (checkPhone == true)
+                {
+                    return BadRequest("SĐT đã tồn tại.");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+            try
+            {
+                var result = await _auth.DangKyUser(model);
+                return Ok(result);
+            } catch(Exception ex)
+            {
+                _logger.LogError(ex, "Error occurred while registering staff.");
+                return StatusCode(500, "Đã xảy ra lỗi khi đăng ký tài khoản.");
+            }
+            
         }
     }
 }
---
+++ BACK-END/DTOs/Auth/DangKyUserDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BACK_END.DTOs.Auth
+{
+    public class DangKyUserDto
+    {
+        [Required(ErrorMessage = "Họ tên không được để trống.")]
+        public string HoTen { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Mật khẩu không được để trống.")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Mật khẩu phải từ 6 đến 20 ký tự.")]
+        public string? MatKhau { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Email không được để trống.")]
+        [EmailAddress(ErrorMessage = "Email không đúng định dạng.")]
+        public string? Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Số điện thoại không được để trống.")]
+        [Phone(ErrorMessage = "Số điện thoại không đúng định dạng.")]
+        [StringLength(10, ErrorMessage = "Số điện thoại không đúng định dạng.")]
+        public string? SoDienThoai { get; set; } = string.Empty;
+    }
+}
