5e700871937530298e7117a272b01d91839aa9d5
TUAN <hoangquoctuans3@gmail.com>
2024-10-01 16:26:50 +0700

    fix-name

--- BACK-END/Models/BoardingHouse_FIle.cs
+++ BACK-END/Models/BoardingHouse_FIle.cs
@@ -3,7 +3,7 @@
     public class BoardingHouse_FIle
     {
         public int Id { get; set; }
-        public int DayTroId { get; set; }
+        public int BoardingHouseId { get; set; }
         public BoardingHouse? BoardingHouse { get; set; }
         public int FileId { get; set; }
         public File? File { get; set; }
