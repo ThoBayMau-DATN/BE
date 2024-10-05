2aea85c277e63f22aa2c7606544eda8b03661004
TUAN <hoangquoctuans3@gmail.com>
2024-10-05 21:09:05 +0700

    fix mapper

--- BACK-END/DTOs/Auth/DangNhapRepository.cs
+++ BACK-END/DTOs/Auth/DangNhapRepository.cs
@@ -4,11 +4,11 @@ namespace BACK_END.DTOs.Auth
 {
     public class DangNhapRepository
     {
-        public string HoTen { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
-        public string? SDT { get; set; } = string.Empty;
-        public string? Anh { get; set; } = string.Empty;
-        public bool TrangThai { get; set; }
+        public string? Phone { get; set; } = string.Empty;
+        public string? Avatar { get; set; } = string.Empty;
+        public bool Status { get; set; }
     }
 
 }
