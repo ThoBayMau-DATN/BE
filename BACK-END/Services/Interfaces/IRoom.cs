fb6d05e5421424f3f79ba39edfa137ab55641dbb
TUAN <hoangquoctuans3@gmail.com>
2024-10-06 22:54:49 +0700

    thêm tìm kiếm theo địa chỉ

--- BACK-END/Services/Interfaces/IRoom.cs
+++ BACK-END/Services/Interfaces/IRoom.cs
@@ -8,6 +8,6 @@ namespace BACK_END.Services.Interfaces
 {
     public interface IRoom
     {
-        Task<List<GetAllRoomRepository>?> GetAllRoom(string sortColumn, string sortOrder, int pageNumber, int pageSize);
+        Task<List<GetAllRoomRepository>?> GetAllRoom(string searchAddress,string sortColumn, string sortOrder, int pageNumber, int pageSize);
     }
 }
