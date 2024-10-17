76aaea1bdd29eeb406ed18e8b269716f7e17d0fa
ho dung <dungho2209@gmail.com>
2024-10-17 16:04:02 +0700

    up thông báo chưa hoàn chỉnh

--- BACK-END/Controllers/NotiController.cs
+++ BACK-END/Controllers/NotiController.cs
@@ -1,4 +1,7 @@
 ﻿using AutoMapper;
+using BACK_END.DTOs.NotiDto;
+using BACK_END.DTOs.Repository;
+using BACK_END.Models;
 using BACK_END.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
@@ -22,7 +25,62 @@ namespace BACK_END.Controllers
         public async Task<IActionResult> getListNotification()
         {
             var notification = await _noti.getAllNotificationAsync();
-            return Ok(notification);
+            return Ok((new ApiResponse<object>
+            {
+                Code = 200,
+                Status = "success",
+                Message = "Lấy danh sách thông báo thành công",
+                Data = notification
+            }));
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> AddNotification([FromBody] addNotification addNotification)
+        {
+            if (ModelState.IsValid) 
+            {
+                var Noti = _mapper.Map<Notification>(addNotification);
+                var CreateNoti = await _noti.addNotificationAsync(Noti);
+
+                return Ok((new ApiResponse<object>
+                {
+                    Code = 200,
+                    Status = "success",
+                    Message = "Lấy danh sách thông báo thành công",
+                    Data = CreateNoti
+                }));
+            }
+            return BadRequest(ModelState);
+            
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateNotification([FromBody] updateNotification updateNotification, int id)
+        {
+            var map = _mapper.Map<Notification>(updateNotification);
+            var NotiUpdate = await _noti.updateNotificationAsync(map, id);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if(NotiUpdate == null)
+            {
+                return NotFound(new ApiResponse<object>
+                {
+                    Code = 404,
+                    Status = "error",
+                    Message = "Không tìm thấy thông báo!!.",
+                    Data = null,
+                });
+            }
+            return Ok((new ApiResponse<object>
+            {
+                Code = 200,
+                Status = "success",
+                Message = "cập nhật danh sách thông báo thành công",
+                Data = NotiUpdate
+            }));
         }
     }
 }
---
+++ BACK-END/Mappers/MapperProfles.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using BACK_END.DTOs.NotiDto;
+using BACK_END.Models;
+
+namespace BACK_END.Mappers
+{
+    public class MapperProfles : Profile
+    {
+        public MapperProfles()
+        {
+            CreateMap<Notification, addNotification>().ReverseMap();
+            CreateMap<Notification, updateNotification>().ReverseMap();
+        }
+    }
+}
