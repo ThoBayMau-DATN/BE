d33735fb8e5a7be577e325f0c0e01e02912ee29a
ho dung <dungho2209@gmail.com>
2024-10-17 21:03:15 +0700

    xong api thông báo

---
+++ BACK-END/DTOs/NotiDto/SendNotificationDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BACK_END.DTOs.NotiDto
+{
+    public class SendNotificationDto
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Type { get; set; }
+        public bool Status { get; set; }
+    }
+}
