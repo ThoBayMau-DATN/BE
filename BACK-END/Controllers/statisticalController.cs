1e42b46d10c5bf160cb9eb987917d7af113aa254
ho dung <dungho2209@gmail.com>
2024-10-18 22:04:18 +0700

    commit thống kê

---
+++ BACK-END/Controllers/statisticalController.cs
@@ -0,0 +1,92 @@
+﻿using BACK_END.DTOs.Repository;
+using BACK_END.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BACK_END.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class statisticalController : Controller
+    {
+        private readonly IStatictical _statictical;
+        public statisticalController(IStatictical statictical)
+        {
+            _statictical = statictical;
+        }
+
+        [HttpGet("count/under-one-million")]
+        public async Task<IActionResult> GetRoomCountUnderOneMillion()
+        {
+            var count = await _statictical.GetRoomCountUnderOneMillionAsync();
+            if (count != null) { 
+                return Ok((new ApiResponse<object>
+            {
+                Code = 200,
+                Status = "success",
+                Message = "Đếm số phòng có mức thuê dưới 1 triệu thành công!!",
+                Data = count
+            }));
+            }
+            else
+            {
+                return BadRequest(new ApiResponse<object>
+                {
+                    Code = 400,
+                    Status = "error",
+                    Message = "Không có dữ liệu trong count!!.",
+                    Data = null,
+                });
+            }
+        }
+
+        [HttpGet("count/last-month")]
+        public async Task<IActionResult> GetUserCountInLastMonth()
+        {
+            var count = await _statictical.GetNewUserCountByTimeCreate();
+            if(count != null) {
+                return Ok((new ApiResponse<object>
+                {
+                    Code = 200,
+                    Status = "success",
+                    Message = "Đếm số người mới tạo tài khoản thành công!!",
+                    Data = count
+                }));
+            }
+            return BadRequest(new ApiResponse<object>
+            {
+                Code = 400,
+                Status = "error",
+                Message = "Không có dữ liệu trong count!!.",
+                Data = null,
+            });
+            
+        }
+
+        [HttpGet("monthly-rental-count")]
+        public async Task<IActionResult> GetMonthlyRentalCount()
+        {
+            
+                var rentalCount = await _statictical.GetMonthlyRentalCountAsync();
+            if (rentalCount != null)
+            {
+                return Ok((new ApiResponse<object>
+                {
+                    Code = 200,
+                    Status = "success",
+                    Message = "Đếm số người thuê trọ thời gian gần đây thành công!!",
+                    Data = rentalCount
+                }));
+            }
+            else
+            {
+                return BadRequest(new ApiResponse<object>
+                {
+                    Code = 400,
+                    Status = "error",
+                    Message = "Không có dữ liệu trong count!!.",
+                    Data = null,
+                });
+            }
+        }
+    }
+}
---
+++ BACK-END/Services/Repositories/StaticticalRepository.cs
@@ -0,0 +1,39 @@
+﻿using BACK_END.Data;
+using BACK_END.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace BACK_END.Services.Repositories
+{
+    public class StaticticalRepository : IStatictical
+    {
+        private readonly BACK_ENDContext _db;
+
+        public StaticticalRepository(BACK_ENDContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<int> GetMonthlyRentalCountAsync()
+        {
+            var now = DateTime.Now;
+            var firstDayOfMonth = new DateTime(now.Year, now.Month, 1);
+
+            var rentalCount = await _db.Rental.CountAsync(r => r.TimeStart >= firstDayOfMonth);
+            return rentalCount;
+        }
+
+        public async Task<int> GetNewUserCountByTimeCreate()
+        {
+            var oneMonthAgo = DateTime.Now.AddMonths(-1);
+            var getNU = await _db.User.CountAsync(r => r.TimeCreated >= oneMonthAgo);
+            return getNU;
+        }
+
+        public async Task<int> GetRoomCountUnderOneMillionAsync()
+        {
+            var statictical = await _db.Room
+        .CountAsync(r => r.Price < 1000000);
+            return statictical;
+        }
+    }
+}
