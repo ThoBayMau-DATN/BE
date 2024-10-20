4c25d19e202e463c755e4d557864d8e6bde6f4d0
ho dung <dungho2209@gmail.com>
2024-10-20 22:06:08 +0700

    doanh thu theo tháng

--- BACK-END/Mappers/MapperProfles.cs
+++ BACK-END/Mappers/MapperProfles.cs
@@ -1,7 +1,9 @@
 ﻿using AutoMapper;
 using BACK_END.DTOs.MotelDto;
 using BACK_END.DTOs.NotiDto;
+using BACK_END.DTOs.StaticDto;
 using BACK_END.Models;
+using System.Globalization;
 
 namespace BACK_END.Mappers
 {
@@ -17,6 +19,10 @@ namespace BACK_END.Mappers
             .ForMember(dest => dest.MotelName, opt => opt.MapFrom(src => src.Name))
             .ForMember(dest => dest.Address, opt => opt.MapFrom(src => src.Address))
             .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status));
+
+            CreateMap<Invoice, RevenueDto>()
+            .ForMember(dest => dest.Month, opt => opt.MapFrom(src => new DateTime(src.TimeCreated.Year, src.TimeCreated.Month, 1)
+                .ToString("MMMM", CultureInfo.InvariantCulture)));
         }
     }
 }
