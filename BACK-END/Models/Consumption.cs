9295c82e7ffdbf70a59ca290395a4635e55f0b86
Do Hoang Phong <hoang@MSIGF63THIN10SC>
2024-10-20 22:26:06 +0700

    add db

---
+++ BACK-END/Models/Consumption.cs
@@ -0,0 +1,12 @@
+﻿namespace BACK_END.Models
+{
+    public class Consumption
+    {
+        public int Id { get; set; }
+        public int Water { get; set; }
+        public int Electricity { get; set; }
+        public DateTime Time { get; set; }
+        public int RoomId { get; set; }
+        public Room? Room { get; set; }
+    }
+}
