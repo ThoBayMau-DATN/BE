8b63a3fa83f4e50a90bebedfc2bfad83f6e8b3b4
Do Hoang Phong <hoang@MSIGF63THIN10SC>
2024-10-20 22:26:06 +0700

    add db

---
+++ BACK-END/Migrations/20241020152538_db2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BACK_END.Migrations
+{
+    /// <inheritdoc />
+    public partial class db2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Consumption",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Water = table.Column<int>(type: "int", nullable: false),
+                    Electricity = table.Column<int>(type: "int", nullable: false),
+                    Time = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    RoomId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Consumption", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Consumption_Room_RoomId",
+                        column: x => x.RoomId,
+                        principalTable: "Room",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Consumption_RoomId",
+                table: "Consumption",
+                column: "RoomId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Consumption");
+        }
+    }
+}
