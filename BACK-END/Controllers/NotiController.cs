f83267d35d63ecb7f0aabd2a1821c9c266b1b975
ho dung <dungho2209@gmail.com>
2024-10-21 08:51:01 +0700

    chỉnh sửa thông báo

--- BACK-END/Controllers/NotiController.cs
+++ BACK-END/Controllers/NotiController.cs
@@ -1,4 +1,7 @@
 ﻿using AutoMapper;
+using BACK_END.DTOs.NotiDto;
+using BACK_END.DTOs.Repository;
+using BACK_END.Models;
 using BACK_END.Services.Interfaces;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -22,91 +25,91 @@ namespace BACK_END.Controllers
 
         }
 
-        //[HttpGet]
-        //public async Task<IActionResult> getListNotification()
-        //{
-        //    var notification = await _noti.getAllNotificationAsync();
-        //    return Ok((new ApiResponse<object>
-        //    {
-        //        Code = 200,
-        //        Status = "success",
-        //        Message = "Lấy danh sách thông báo thành công",
-        //        Data = notification
-        //    }));
-        //}
+        [HttpGet]
+        public async Task<IActionResult> getListNotification()
+        {
+            var notification = await _noti.getAllNotificationAsync();
+            return Ok((new ApiResponse<object>
+            {
+                Code = 200,
+                Status = "success",
+                Message = "Lấy danh sách thông báo thành công",
+                Data = notification
+            }));
+        }
 
-        //[HttpPost]
+        [HttpPost]
 
-        //public async Task<IActionResult> AddNotification([FromBody] addNotification addNotification)
-        //{
-        //    if (ModelState.IsValid) 
-        //    {
-        //        var Noti = _mapper.Map<Notification>(addNotification);
-        //        var CreateNoti = await _noti.addNotificationAsync(Noti);
+        public async Task<IActionResult> AddNotification([FromBody] addNotification addNotification)
+        {
+            if (ModelState.IsValid)
+            {
+                var Noti = _mapper.Map<Notification>(addNotification);
+                var CreateNoti = await _noti.addNotificationAsync(Noti);
 
-        //        return Ok((new ApiResponse<object>
-        //        {
-        //            Code = 200,
-        //            Status = "success",
-        //            Message = "Lấy danh sách thông báo thành công",
-        //            Data = CreateNoti
-        //        }));
-        //    }
-        //    return BadRequest(ModelState);
+                return Ok((new ApiResponse<object>
+                {
+                    Code = 200,
+                    Status = "success",
+                    Message = "Lấy danh sách thông báo thành công",
+                    Data = CreateNoti
+                }));
+            }
+            return BadRequest(ModelState);
 
-        //}
+        }
 
-        //[HttpPut("{id}")]
-        //public async Task<IActionResult> UpdateNotification([FromBody] updateNotification updateNotification, int id)
-        //{
-        //    var map = _mapper.Map<Notification>(updateNotification);
-        //    var NotiUpdate = await _noti.updateNotificationAsync(map, id);
-        //    if (!ModelState.IsValid)
-        //    {
-        //        return BadRequest(ModelState);
-        //    }
-        //    if(NotiUpdate == null)
-        //    {
-        //        return NotFound(new ApiResponse<object>
-        //        {
-        //            Code = 404,
-        //            Status = "error",
-        //            Message = "Không tìm thấy thông báo!!.",
-        //            Data = null,
-        //        });
-        //    }
-        //    return Ok((new ApiResponse<object>
-        //    {
-        //        Code = 200,
-        //        Status = "success",
-        //        Message = "cập nhật danh sách thông báo thành công",
-        //        Data = NotiUpdate
-        //    }));
-        //}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateNotification([FromBody] updateNotification updateNotification, int id)
+        {
+            var map = _mapper.Map<Notification>(updateNotification);
+            var NotiUpdate = await _noti.updateNotificationAsync(map, id);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (NotiUpdate == null)
+            {
+                return NotFound(new ApiResponse<object>
+                {
+                    Code = 404,
+                    Status = "error",
+                    Message = "Không tìm thấy thông báo!!.",
+                    Data = null,
+                });
+            }
+            return Ok((new ApiResponse<object>
+            {
+                Code = 200,
+                Status = "success",
+                Message = "cập nhật danh sách thông báo thành công",
+                Data = NotiUpdate
+            }));
+        }
 
-        //[HttpPost("SendByRole/{roleName}")]
-        //public async Task<IActionResult> SendNotification([FromBody] SendNotificationDto sendNotificationDto, string roleName)
-        //{
-        //    // Kiểm tra nếu yêu cầu hợp lệ
-        //    if (ModelState.IsValid)
-        //    {
-        //        var map = _mapper.Map<Notification>(sendNotificationDto);
-        //        var sendnoti =  await _noti.SendNotificationToRolesAsync(roleName, map);
-        //        return Ok((new ApiResponse<object>
-        //        {
-        //            Code = 200,
-        //            Status = "success",
-        //            Message = "Gửi thông báo thành công",
-        //            Data = sendnoti
-        //        }));
-        //    }
-        //    return BadRequest(new ApiResponse<object>
-        //    {
-        //        Code = 400,
-        //        Status = "error",
-        //        Message = "Gửi không thành công!!.",
-        //        Data = null,
-        //    });
-        //}
+        [HttpPost("SendByRole/{roleName}")]
+        public async Task<IActionResult> SendNotification([FromBody] SendNotificationDto sendNotificationDto, string roleName)
+        {
+            // Kiểm tra nếu yêu cầu hợp lệ
+            if (ModelState.IsValid)
+            {
+                var map = _mapper.Map<Notification>(sendNotificationDto);
+                var sendnoti = await _noti.SendNotificationToRolesAsync(roleName, map);
+                return Ok((new ApiResponse<object>
+                {
+                    Code = 200,
+                    Status = "success",
+                    Message = "Gửi thông báo thành công",
+                    Data = sendnoti
+                }));
+            }
+            return BadRequest(new ApiResponse<object>
+            {
+                Code = 400,
+                Status = "error",
+                Message = "Gửi thông báo thất bại!!.",
+                Data = null,
+            });
+        }
     }
 }
--- BACK-END/DTOs/NotiDto/addNotification.cs
+++ BACK-END/DTOs/NotiDto/addNotification.cs
@@ -1,28 +1,24 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BACK_END.DTOs.NotiDto
 {
     public class addNotification
     {
         [Required(ErrorMessage = "Loại thông báo không được để trống")]
-        [StringLength(50,ErrorMessage ="Loại thông báo không quá 50 kí tự")]
-        public string Type { get; set; } = string.Empty;
+        public int Type { get; set; }
 
         [Required(ErrorMessage = "Tiêu đề không được để trống")]
-        [StringLength(100, ErrorMessage = "Tiêu đề không được quá 100 kí tự")]
-        public string Title { get; set; } = string.Empty;
+        [StringLength(100, ErrorMessage = "Nội dung không quá 100 kí tự")]
+        public string? Title { get; set; }
 
         [Required(ErrorMessage = "Nội dung không được để trống")]
         [StringLength(500, ErrorMessage = "Nội dung không quá 500 kí tự")]
-        public string Content { get; set; } = string.Empty;
+        public string? Content { get; set; }
 
-        [Range(0, 1, ErrorMessage =("Trạng thái không hợp lệ"))]
-        [Column(TypeName = "tinyint")]
-        public byte Status { get; set; }
+        [Range(0, 4, ErrorMessage = ("Trạng thái không hợp lệ"))]
+        [Required(ErrorMessage = "Nội dung không được để trống")]
+        public int Status { get; set; }
 
-        [Required]
-        public int UserId { get; set; }
 
     }
 }
