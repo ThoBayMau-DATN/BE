e0c02562cbd6c4558201995c9983259cda883619
Do Hoang Phong <hoang@MSIGF63THIN10SC>
2024-10-21 15:29:15 +0700

    edit ticket

--- BACK-END/DTOs/Ticket/Create.cs
+++ BACK-END/DTOs/Ticket/Create.cs
@@ -1,11 +1,16 @@
-﻿namespace BACK_END.DTOs.Ticket
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BACK_END.DTOs.Ticket
 {
     public class Create
     {
-        public string Type { get; set; } = string.Empty;
-        public string Title { get; set; } = string.Empty;
-        public string Content { get; set; } = string.Empty;
+        public int Type { get; set; }
+        [Required(ErrorMessage = "Tiêu đề không được để trống")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Nội dung không được để trống")]
+        public string Content { get; set; }
         public int Status { get; set; }
+        public string? Receiver { get; set; }
         public int UserId { get; set; }
         public int MotelId { get; set; }
     }
