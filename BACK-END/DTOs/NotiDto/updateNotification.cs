461fc93f1973709776283fee92e2540b02e83394
ho dung <dungho2209@gmail.com>
2024-10-25 10:16:16 +0700

    update trường thêm sửa thông báo dto

--- BACK-END/DTOs/NotiDto/updateNotification.cs
+++ BACK-END/DTOs/NotiDto/updateNotification.cs
@@ -14,8 +14,5 @@ namespace BACK_END.DTOs.NotiDto
         [StringLength(500, ErrorMessage = "Nội dung không quá 500 kí tự")]
         public string? Content { get; set; }
 
-        [Range(0, 4, ErrorMessage = ("Trạng thái không hợp lệ"))]
-        [Required(ErrorMessage = "Nội dung không được để trống")]
-        public byte Status { get; set; }
     }
 }
