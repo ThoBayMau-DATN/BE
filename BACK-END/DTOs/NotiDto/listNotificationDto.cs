872dac67e0874e1c96cf6042386d94c0a75de963
ho dung <dungho2209@gmail.com>
2024-11-04 00:07:42 +0700

    update get all noti

---
+++ BACK-END/DTOs/NotiDto/listNotificationDto.cs
@@ -0,0 +1,13 @@
+﻿namespace BACK_END.DTOs.NotiDto
+{
+    public class listNotificationDto
+    {
+
+            public int TotalCount { get; set; }
+            public int TotalPages { get; set; }
+            public int PageNumber { get; set; }
+            public int PageSize { get; set; }
+            public List<BACK_END.Models.Notification>? Items { get; set; }
+
+    }
+}
