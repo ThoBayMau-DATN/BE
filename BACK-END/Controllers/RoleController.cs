a2df8daa2366ec91769770a462b3664a39ff4559
Ro0190124 <128989515+Ro0190124@users.noreply.github.com>
2024-11-10 13:41:13 +0700

    Add role
    Thêm role cho user

---
+++ BACK-END/Controllers/RoleController.cs
@@ -0,0 +1,57 @@
+﻿using BACK_END.DTOs.Repository;
+using BACK_END.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BACK_END.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+        private readonly IRole _role;
+        public RoleController(IRole role) {
+            _role = role;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetRole()
+        {
+            try
+            {
+                var roles = await _role.GetRole();
+                if (roles == null || !roles.Any())
+                {
+                    return NotFound(new ApiResponse<object>
+                    {
+                        Code = 404,
+                        Status = "error",
+                        Message = "Không tìm thấy vai trò",
+                        Data = null
+                    });
+                }
+                else
+                {
+                    return Ok(new ApiResponse<object>
+                    {
+                        Code = 200,
+                        Status = "success",
+                        Message = "Lấy danh sách vai trò thành công",
+                        Data = roles
+                    });
+                }
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new ApiResponse<object>
+                {
+                    Code = 500,
+                    Status = "error",
+                    Message = e.Message,
+                    Data = null
+                });
+            }
+            
+        }
+
+    }
+}
---
+++ BACK-END/Services/Interfaces/IRole.cs
@@ -0,0 +1,10 @@
+﻿using BACK_END.DTOs.RoleDto;
+
+namespace BACK_END.Services.Interfaces
+{
+    public interface IRole
+    {
+        //get all of Role
+        public Task<List<GetRole>> GetRole();
+    }
+}
