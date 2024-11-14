c89a4982cf65c6aea86091adb234b53e6403f8b0
Do Hoang Phong <hoang@MSIGF63THIN10SC>
2024-11-14 10:27:35 +0700

    update database sql

--- BACK-END/Data/DependencyInjection.cs
+++ BACK-END/Data/DependencyInjection.cs
@@ -10,15 +10,15 @@ namespace BACK_END.Data
         {
             services.AddScoped<IAuth, AuthRepository>();
             services.AddScoped<TokenService>();
-            services.AddScoped<IRoom, RoomRepository>();
-            services.AddScoped<INoti, NofRespository>();
-            services.AddScoped<IStatictical, StaticticalRepository>();
+            //services.AddScoped<IRoom, RoomRepository>();
+            //services.AddScoped<INoti, NofRespository>();
+            //services.AddScoped<IStatictical, StaticticalRepository>();
             services.AddScoped<IUser, UserRepository>();
             services.AddScoped<ITicket, TicketRepository>();
             services.AddScoped<FirebaseStorageService>();
             services.AddScoped<IHome, HomeRespository>();
             services.AddScoped<IRole, RoleRepository>();
-            services.AddScoped<IGetTro, MainRespository>();
+            //services.AddScoped<IGetTro, MainRespository>();
         }
     }
 }
