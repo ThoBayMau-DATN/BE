47e29e47a6be34195bacaaaaa9accc7c2cf87afc
ho dung <dungho2209@gmail.com>
2024-11-16 22:11:57 +0700

    sửa noti

--- BACK-END/Mappers/MapperProfles.cs
+++ BACK-END/Mappers/MapperProfles.cs
@@ -1,56 +1,54 @@
-﻿//using AutoMapper;
-//using BACK_END.DTOs.MotelDto;
-//using BACK_END.DTOs.NotiDto;
-//using BACK_END.DTOs.RoomDto;
-//using BACK_END.Models;
+﻿using AutoMapper;
+using BACK_END.DTOs.NotiDto;
+using BACK_END.Models;
 
-//namespace BACK_END.Mappers
-//{
-//    public class MapperProfles : Profile
-//    {
-//        public MapperProfles()
-//        {
-//            CreateMap<Notification, addNotification>().ReverseMap();
-//            CreateMap<Notification, updateNotification>().ReverseMap();
-//            CreateMap<Notification, SendNotificationDto>().ReverseMap();
-//            CreateMap<Services.MyServices.PagedList<Notification>, DTOs.NotiDto.listNotificationDto>()
-//            .ForMember(dest => dest.TotalCount, opt => opt.MapFrom(src => src.TotalCount))
-//            .ForMember(dest => dest.TotalPages, opt => opt.MapFrom(src => src.TotalPages))
-//            .ForMember(dest => dest.PageNumber, opt => opt.MapFrom(src => src.CurrentPage))
-//            .ForMember(dest => dest.PageSize, opt => opt.MapFrom(src => src.PageSize))
-//            .ForMember(dest => dest.Items, opt => opt.MapFrom(src => src));
+namespace BACK_END.Mappers
+{
+    public class MapperProfles : Profile
+    {
+        public MapperProfles()
+        {
+            CreateMap<Notification, addNotification>().ReverseMap();
+            CreateMap<Notification, updateNotification>().ReverseMap();
+            CreateMap<Notification, SendNotificationDto>().ReverseMap();
+            CreateMap<Services.MyServices.PagedList<Notification>, DTOs.NotiDto.listNotificationDto>()
+            .ForMember(dest => dest.TotalCount, opt => opt.MapFrom(src => src.TotalCount))
+            .ForMember(dest => dest.TotalPages, opt => opt.MapFrom(src => src.TotalPages))
+            .ForMember(dest => dest.PageNumber, opt => opt.MapFrom(src => src.CurrentPage))
+            .ForMember(dest => dest.PageSize, opt => opt.MapFrom(src => src.PageSize))
+            .ForMember(dest => dest.Items, opt => opt.MapFrom(src => src));
 
-//            CreateMap<Room, GetRoomById>()
-//                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
-//                .ForMember(dest => dest.RoomNumber, opt => opt.MapFrom(src => src.RoomNumber))
-//                .ForMember(dest => dest.Area, opt => opt.MapFrom(src => src.Area))
-//                .ForMember(dest => dest.Price, opt => opt.MapFrom(src => src.Price));
+            //CreateMap<Room, GetRoomById>()
+            //    .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+            //    .ForMember(dest => dest.RoomNumber, opt => opt.MapFrom(src => src.RoomNumber))
+            //    .ForMember(dest => dest.Area, opt => opt.MapFrom(src => src.Area))
+            //    .ForMember(dest => dest.Price, opt => opt.MapFrom(src => src.Price));
 
-//            CreateMap<Motel, MotelAvailabilityDTO>()
-//            .ForMember(dest => dest.MotelName, opt => opt.MapFrom(src => src.Name))
-//            .ForMember(dest => dest.Address, opt => opt.MapFrom(src => src.Address))
-//            .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status));
+            //CreateMap<Motel, MotelAvailabilityDTO>()
+            //.ForMember(dest => dest.MotelName, opt => opt.MapFrom(src => src.Name))
+            //.ForMember(dest => dest.Address, opt => opt.MapFrom(src => src.Address))
+            //.ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status));
 
-//            //ticket
-//            CreateMap<Models.Ticket, DTOs.Ticket.Tickets>().ReverseMap();
+            ////ticket
+            //CreateMap<Models.Ticket, DTOs.Ticket.Tickets>().ReverseMap();
 
-//            //infoticket
-//            CreateMap<Models.Ticket, DTOs.Ticket.Infoticket>()
-//            .ForMember(dest => dest.Imgs, opt => opt.MapFrom(src => src.Images.Select(i => i.Link)))
-//            .ForMember(dest => dest.UserName, opt => opt.MapFrom(x => x.User.FullName));
+            ////infoticket
+            //CreateMap<Models.Ticket, DTOs.Ticket.Infoticket>()
+            //.ForMember(dest => dest.Imgs, opt => opt.MapFrom(src => src.Images.Select(i => i.Link)))
+            //.ForMember(dest => dest.UserName, opt => opt.MapFrom(x => x.User.FullName));
 
-//            CreateMap<Models.User, DTOs.Ticket.Receiver>().ReverseMap();
+            //CreateMap<Models.User, DTOs.Ticket.Receiver>().ReverseMap();
 
-//            ///map pagelist
-//            CreateMap<Services.MyServices.PagedList<Ticket>, DTOs.Ticket.TicketPagination>()
-//            .ForMember(dest => dest.TotalCount, opt => opt.MapFrom(src => src.TotalCount))
-//            .ForMember(dest => dest.TotalPages, opt => opt.MapFrom(src => src.TotalPages))
-//            .ForMember(dest => dest.PageNumber, opt => opt.MapFrom(src => src.CurrentPage))
-//            .ForMember(dest => dest.PageSize, opt => opt.MapFrom(src => src.PageSize))
-//            .ForMember(dest => dest.Items, opt => opt.MapFrom(src => src));
+            /////map pagelist
+            //CreateMap<Services.MyServices.PagedList<Ticket>, DTOs.Ticket.TicketPagination>()
+            //.ForMember(dest => dest.TotalCount, opt => opt.MapFrom(src => src.TotalCount))
+            //.ForMember(dest => dest.TotalPages, opt => opt.MapFrom(src => src.TotalPages))
+            //.ForMember(dest => dest.PageNumber, opt => opt.MapFrom(src => src.CurrentPage))
+            //.ForMember(dest => dest.PageSize, opt => opt.MapFrom(src => src.PageSize))
+            //.ForMember(dest => dest.Items, opt => opt.MapFrom(src => src));
 
 
 
-//        }
-//    }
-//}
+        }
+    }
+}
--- BACK-END/Services/Repositories/NofRespository.cs
+++ BACK-END/Services/Repositories/NofRespository.cs
@@ -1,124 +1,124 @@
-﻿//using AutoMapper;
-//using BACK_END.Data;
-//using BACK_END.DTOs.NotiDto;
-//using BACK_END.DTOs.Ticket;
-//using BACK_END.Models;
-//using BACK_END.Services.Interfaces;
-//using BACK_END.Services.MyServices;
-//using Microsoft.AspNetCore.Identity;
-//using Microsoft.EntityFrameworkCore;
+﻿using AutoMapper;
+using BACK_END.Data;
+using BACK_END.DTOs.NotiDto;
+using BACK_END.DTOs.Ticket;
+using BACK_END.Models;
+using BACK_END.Services.Interfaces;
+using BACK_END.Services.MyServices;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
-//namespace BACK_END.Services.Repositories
-//{
-//    public class NofRespository : INoti
-//    {
-//        private readonly BACK_ENDContext _db;
-//        private readonly UserManager<IdentityUser> _userManager;
-//        private readonly IMapper _mapper;
+namespace BACK_END.Services.Repositories
+{
+    public class NofRespository : INoti
+    {
+        private readonly BACK_ENDContext _db;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IMapper _mapper;
 
-//        public NofRespository(BACK_ENDContext db, UserManager<IdentityUser> userManager, IMapper mapper)
-//        {
-//            _db = db;
-//            _userManager = userManager;
-//            _mapper = mapper;
-//        }
+        public NofRespository(BACK_ENDContext db, UserManager<IdentityUser> userManager, IMapper mapper)
+        {
+            _db = db;
+            _userManager = userManager;
+            _mapper = mapper;
+        }
 
-//        public async Task<Notification> addNotificationAsync(Notification notification)
-//        {
-//            notification.Status = 1;
-//            await _db.AddAsync(notification);
-//            await _db.SaveChangesAsync();
-//            return notification;
-//        }
+        public async Task<Notification> addNotificationAsync(Notification notification)
+        {
+            notification.Status = false;
+            await _db.AddAsync(notification);
+            await _db.SaveChangesAsync();
+            return notification;
+        }
 
-//        public async Task<listNotificationDto> GetAllNotiAsync(NotiQuery notiQuery)
-//        {
-//            IQueryable<Notification> data = _db.Notification;
+        public async Task<listNotificationDto> GetAllNotiAsync(NotiQuery notiQuery)
+        {
+            IQueryable<Notification> data = _db.Notification;
 
-//            if (notiQuery.Status > 0)
-//            {
-//                data = data.Where(x => x.Status == notiQuery.Status);
-//            }
-//            if (!string.IsNullOrEmpty(notiQuery.Search))
-//            {
-//                data = data.Where(x => x.Title.ToLower().Contains(notiQuery.Search.ToLower()));
-//            }
+            if (notiQuery.Status)
+            {
+                data = data.Where(x => x.Status == notiQuery.Status);
+            }
+            if (!string.IsNullOrEmpty(notiQuery.Search))
+            {
+                data = data.Where(x => x.Title.ToLower().Contains(notiQuery.Search.ToLower()));
+            }
 
-//            var page = await PagedList<Notification>.CreateAsync(data, notiQuery.PageNumber, notiQuery.PageSize);
+            var page = await PagedList<Notification>.CreateAsync(data, notiQuery.PageNumber, notiQuery.PageSize);
 
-//            var paginationResult = _mapper.Map<listNotificationDto>(page);
-//            return paginationResult;
-//        }
+            var paginationResult = _mapper.Map<listNotificationDto>(page);
+            return paginationResult;
+        }
 
-//        public async Task<List<Notification>> GetSentNotificationsAsync(int userId)
-//        {
-//            return await _db.User_Notification
-//                .Where(un => un.UserId == userId && un.Notification!.Status == 2)
-//                .Include(un => un.Notification)
-//                .Select(un => un.Notification)
-//                .ToListAsync();
-//        }
+        public async Task<List<Notification>> GetSentNotificationsAsync(int userId)
+        {
+            return await _db.User_Notification
+                .Where(un => un.UserId == userId && un.Notification!.Status == true)
+                .Include(un => un.Notification)
+                .Select(un => un.Notification)
+                .ToListAsync();
+        }
 
-//        public async Task<User?> GetUserByEmailAsync(string email)
-//        {
-//            return await _db.User.FirstOrDefaultAsync(u => u.Email == email);
-//        }
+        public async Task<User?> GetUserByEmailAsync(string email)
+        {
+            return await _db.User.FirstOrDefaultAsync(u => u.Email == email);
+        }
 
-//        public async Task<IEnumerable<IdentityUser>> GetUsersByRoleAsync(string roleName)
-//        {
-//            return await _userManager.GetUsersInRoleAsync(roleName);
-//        }
-//        public async Task<Notification?> SendNotificationToRolesByIdAsync(int notificationId, string roleName)
-//        {
-//            var notification = await _db.Notification.FindAsync(notificationId);
-//            if (notification == null)
-//            {
-//                return null;
-//            }
+        public async Task<IEnumerable<IdentityUser>> GetUsersByRoleAsync(string roleName)
+        {
+            return await _userManager.GetUsersInRoleAsync(roleName);
+        }
+        public async Task<Notification?> SendNotificationToRolesByIdAsync(int notificationId, string roleName)
+        {
+            var notification = await _db.Notification.FindAsync(notificationId);
+            if (notification == null)
+            {
+                return null;
+            }
 
-//            notification.Status = 2;
-//            _db.Notification.Update(notification);
+            notification.Status = true;
+            _db.Notification.Update(notification);
 
-//            var usersInRole = await GetUsersByRoleAsync(roleName);
-//            var userEmails = new List<User>();
-//            foreach (var e in usersInRole)
-//            {
-//                var users = _db.User.Where(x => x.Email == e.Email);
-//                if (users.Any())
-//                {
-//                    userEmails.AddRange(users);
-//                }
-//            }
-//            foreach (var user in userEmails)
-//            {
-//                var userNotification = new User_Notification
-//                {
-//                    UserId = user.Id,
-//                    NotificationId = notification.Id,
-//                };
-//                await _db.AddAsync(userNotification);
-//            }
-//            await _db.SaveChangesAsync();
+            var usersInRole = await GetUsersByRoleAsync(roleName);
+            var userEmails = new List<User>();
+            foreach (var e in usersInRole)
+            {
+                var users = _db.User.Where(x => x.Email == e.Email);
+                if (users.Any())
+                {
+                    userEmails.AddRange(users);
+                }
+            }
+            foreach (var user in userEmails)
+            {
+                var userNotification = new User_Notification
+                {
+                    UserId = user.Id,
+                    NotificationId = notification.Id,
+                };
+                await _db.AddAsync(userNotification);
+            }
+            await _db.SaveChangesAsync();
 
-//            return notification;
-//        }
-
-
+            return notification;
+        }
 
 
-//        public async Task<Notification> updateNotificationAsync(Notification notification, int id)
-//        {
-//            var notiExist = await _db.Notification.FirstOrDefaultAsync(x => x.Id == id);
-//            if (notiExist == null)
-//            {
-//                return null;
-//            }
-//            notiExist.Type = notification.Type;
-//            notiExist.Title = notification.Title;
-//            notiExist.Content = notification.Content;
-//            notiExist.Status = 0;
-//            await _db.SaveChangesAsync();
-//            return notification;
-//        }
-//    }
-//}
+
+
+        public async Task<Notification> updateNotificationAsync(Notification notification, int id)
+        {
+            var notiExist = await _db.Notification.FirstOrDefaultAsync(x => x.Id == id);
+            if (notiExist == null)
+            {
+                return null;
+            }
+            notiExist.Type = notification.Type;
+            notiExist.Title = notification.Title;
+            notiExist.Content = notification.Content;
+            notiExist.Status = false;
+            await _db.SaveChangesAsync();
+            return notification;
+        }
+    }
+}
