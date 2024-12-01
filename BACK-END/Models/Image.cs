0e2289c255fd0abcd82f6a2b33124e7d76024724
Do Hoang Phong <hoang@MSIGF63THIN10SC>
2024-12-01 15:47:03 +0700

    add name

--- BACK-END/Models/Image.cs
+++ BACK-END/Models/Image.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public string? Link { get; set; }
+        public string? Name { get; set; }
         public string? Type { get; set; }
         public int? Room_TypeId { get; set; }
         public virtual Room_Type? Room_Type { get; set; }
