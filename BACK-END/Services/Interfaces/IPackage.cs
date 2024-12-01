dcdaec849031e1b20fb5254f31fad3cd20410f04
Ro0190124 <128989515+Ro0190124@users.noreply.github.com>
2024-12-01 21:46:26 +0700

    Register Package

---
+++ BACK-END/Services/Interfaces/IPackage.cs
@@ -0,0 +1,13 @@
+﻿using BACK_END.DTOs.PackageDto;
+namespace BACK_END.Services.Interfaces
+{
+    public interface IPackage
+    {
+       
+        public Task<bool> RegisterPackageDto(RegisterPackageDto registerPackageDto);
+        public Task<PackageDto> UnregisterPackageDto(int id);
+        public Task<List<PackageDto>> GetAllPackageDto(PackageGetAllDto package);
+        public Task<PackageDto> GetPackageDtoById(int id);
+        public Task<PackageDto> CheckPackage(string token);
+    }
+}
