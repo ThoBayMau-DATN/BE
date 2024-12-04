9abbdc618c33c87b5cef369f28d216370c127184
Ro0190124 <128989515+Ro0190124@users.noreply.github.com>
2024-12-04 20:28:35 +0700

    Revenue
    Doanh thu + thống kê tài khoản

--- BACK-END/Services/Repositories/StaticticalRepository.cs
+++ BACK-END/Services/Repositories/StaticticalRepository.cs
@@ -1,19 +1,29 @@
 ﻿using BACK_END.Data;
 using BACK_END.DTOs.StaticDto;
+using BACK_END.DTOs.UserDto;
+using BACK_END.Models;
 using BACK_END.Services.Interfaces;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace BACK_END.Services.Repositories
 {
     public class StaticticalRepository : IStatictical
     {
         private readonly BACK_ENDContext _db;
+        private readonly IAuth _auth;
+        private readonly UserManager<IdentityUser> _userManager;
 
 
-        public StaticticalRepository(BACK_ENDContext db)
+        public StaticticalRepository(BACK_ENDContext db, IAuth auth, UserManager<IdentityUser> userManager)
         {
             _db = db;
+            _auth = auth;
+            _userManager = userManager;
         }
+
+        
 
         //public async Task<List<Invoice>> GetAllAsync()
         //{
@@ -66,6 +76,132 @@ namespace BACK_END.Services.Repositories
                 .ToListAsync();
 
             return motelsWithEmptyRooms;
+        }
+
+
+        //doanh thu admin
+        public async Task<List<RevenueAdmin>> MonthlyRevenue(selecRevenueAdmin item)
+        {
+            //check token
+            try
+            {
+                if( item.token != null){
+
+                    var isUser = await _auth.GetUserByToken(item.token);
+
+                    if (isUser.Role.ToUpper() == "ADMIN")
+                    {
+                        var currentDate = DateTime.Now;
+                        //thống kê doanh thu
+                        var monthlyRevenue = await _db.Package_User
+                            .Where(x => x.CreateDate.HasValue
+                          )
+                            .Join(_db.Package, pu => pu.PackageId, p => p.Id, (pu, p) => new { pu, p })
+                            .GroupBy(x => new { x.pu.CreateDate.Value.Month, x.pu.CreateDate.Value.Year })
+                            .Select(x => new RevenueAdmin
+                            {
+                                Month = x.Key.Month,
+                                Year = x.Key.Year,
+                                TotalRevenue  = x.Sum(x => x.p.Price)
+                            }).ToListAsync();
+
+                        return monthlyRevenue;
+
+
+
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+               
+            }
+            catch(UnauthorizedAccessException ex)
+            {
+                throw new Exception("Không có quyền truy cập.", ex);
+            }
+
+            return null;
+        }
+        //count Account
+        public async Task<List<MonthlyCountAccount>> CountAccount(FormCountAccount item)
+        {
+            try
+            {
+                if (item.token != null) {
+                    var isUser = await _auth.GetUserByToken(item.token);
+                    if (isUser.Role.ToUpper() == "ADMIN")
+                    {
+                        //lấy danh sách người dùng
+                        var users = await _db.User.ToListAsync();
+                        //Truy vấn danh sách người dùng bảng ASP
+
+
+                        Dictionary<string, string> userRoles = new Dictionary<string, string>();
+
+                        IList<IdentityUser> iusers = await _userManager.Users.ToListAsync(); // tất cả người dùng trong identity
+                        for (int i = 0; i < iusers.Count; i++)
+                        {
+                            var roles = await _userManager.GetRolesAsync(iusers[i]);
+                            for (int j = 0; j < roles.Count; j++)
+                            {
+                                Console.WriteLine(roles[j]);
+                                userRoles.Add(iusers[i].Email, roles[0]);
+                            }
+                        }
+                        // danh sách TẤt CẢ các người dùng trong identity
+                        Dictionary<string, User> userDict = new Dictionary<string, User>();
+                        foreach (User user in _db.User.AsQueryable())
+                        {
+                            userDict.Add(user.Email, user);
+                            Console.WriteLine("Email người dùng: " + user.Email);
+                        }
+
+                        var usersWithRoles = users.Select(user => new GetAllUserRepositoryDto
+                        {
+                            Id = userDict[user.Email].Id,
+                            Email = user.Email,
+                            FullName = userDict[user.Email].FullName,
+                            Phone = userDict[user.Email].Phone,
+                            Avatar = userDict[user.Email].Avatar,
+                            TimeCreated = userDict[user.Email].CreateDate,
+                            Status = userDict[user.Email].Status,
+                            Role = userRoles.GetValueOrDefault(user.Email, "")
+                        });
+
+
+
+                        if (!string.IsNullOrEmpty(item.role))
+                        {
+                            usersWithRoles = usersWithRoles.Where(x => x.Role.ToUpper() == item.role.ToUpper());
+                        }
+                        var monthlyCounts = usersWithRoles
+                       .GroupBy(x => new { x.TimeCreated.Year, x.TimeCreated.Month })
+                       .Select(g => new MonthlyCountAccount
+                       {
+                           Year = g.Key.Year,
+                           Month = g.Key.Month,
+                           TotalAccount = g.Count()
+                       })
+                       .OrderBy(x => x.Year)
+                       .ThenBy(x => x.Month)
+                       .ToList();
+
+                        return monthlyCounts;
+                    }
+                }
+                else
+                {
+
+                }
+
+            }
+            catch
+            {
+
+            }
+            throw new NotImplementedException();
         }
     }
 }
